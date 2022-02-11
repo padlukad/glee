4372f0e9f68f33f4bb7475f122a9bb3019ae66c8
Ihor <72511454+padlukad@users.noreply.github.com>
2022-02-11 14:58:09 +0200

    added shop list and shop list2

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,32 @@
 $(function() {
+
+    $(".filter-price__input").ionRangeSlider({
+        type: "double",
+        prefix: "$",
+        onStart: function (data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+        onChange: function (data) {
+            $('.filter-price__from').text(data.from);
+            $('.filter-price__to').text(data.to);
+        },
+    });
+
+    $(".star").rateYo({
+        starWidth: "11px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+        readOnly: true
+    });
+
+    $(".star-two").rateYo({
+        starWidth: "16px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+        readOnly: true
+    });
+
     $('.top-slider__slick').slick({
         dots: true,
         arrows: false,
